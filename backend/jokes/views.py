1b003c0000799bc51ece04183d0b637b713d3c92
JoseDev <joseluisdp297@gmail.com>
2024-07-24 13:08:22 +0100

    Create API view

--- backend/jokes/views.py
+++ backend/jokes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# Importing everything needed for defining views
+from .models import Joke
+from .serializers import JokeSerializer
+from rest_framework import generics
 
 # Create your views here.
+# Class-based view for listing and creating jokes
+
+
+class JokeListCreate(generics.ListCreateAPIView):
+    # Define the queryset to be used for retrieving data
+    queryset = Joke.objects.all()
+
+    # Define the serializer class to be used for converting data
+    serializer_class = JokeSerializer
